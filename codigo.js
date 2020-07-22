038fd8009fadc78e157bd643ee5940dd42baa163
Pedro Leonardo <pedrolts16@gmail.com>
2020-07-22 17:21:56 -0300

    Implementando async/await e fazendo estilizacao

--- codigo.js
+++ codigo.js
@@ -14,7 +14,23 @@ function adcRepos(){
         liElement[i].textContent = repos[i];
     }
 }
-
+async function BuscaApi(user){
+    // caso responda com sucesso :
+    try{
+        const response = await axios.get(`https://api.github.com/users/${user}/repos`);
+        const data = response.data;
+        for(let i = 0; i < data.length; i++){
+            // cada repositorio na api é adicionado no vetor repos
+            repos.push(response.data[i].name);
+        }
+        // executa a função para adicionar repositorios na lista
+        adcRepos();
+    // caso responda com um erro
+    }catch(error){
+        console.warn(error);
+        alert(error);
+    };
+};
 btnElement.onclick = () => {
     const ulTag = document.querySelector("ul");
     // esvazia a lista caso existam itens
@@ -26,24 +42,8 @@ btnElement.onclick = () => {
     // cria uma item na lista informando que a consulta esta carregando
     liElement.textContent = "...carregando...";
     // inicia a consulta na api
-    const searchUser = axios.get(`https://api.github.com/users/${inputElement.value}/repos`)
-    // caso responda com sucesso :
-        .then(response => {
-            const data = response.data;
-            for(let i = 0; i < data.length; i++){
-                // cada repositorio na api é adicionado no vetor repos
-                repos.push(response.data[i].name);
-            }
-        // executa a função para adicionar repositorios na lista
-        adcRepos();
-        })
-        // caso responda com um erro
-        .catch(error => {
-            // cria um aviso no console e um alerta na tela
-            console.warn(error);
-            alert(error);
-        })
-        // esvazia o vetor de repositorio para uma nova consulta
-        repos = [];
-        inputElement.value = "";
+    BuscaApi(inputElement.value)    
+    // esvazia o vetor de repositorio para uma nova consulta
+    repos = [];
+    inputElement.value = "";
 }
