b065d610d0f8b0962258bec3dfd96449ad6c3ff4
Pedro Leonardo <pedrolts16@gmail.com>
2020-07-23 03:22:18 -0300

    Prossenguindo com estudos

--- src/main.js
+++ src/main.js
@@ -1,41 +1,96 @@
-import axios from "axios";
+import api from "./api";
 class App{
     // metodo e instanciado assim que a classe e chamada
     constructor(){
         // array que recebertodos os repositorios
         this.repositorio =[];
+        // array que recebe usuarios
+        this.user = [];
         // seleciona a tag form
         this.form = document.getElementById("repo-form");
         this.list = document.getElementById("repo-list");
+        this.repoUserInput = document.getElementById("repo-user");
+        this.repository = document.getElementById("repository");
+        this.input
         // instancia o metodo
         this.registerHandler();
 
     }
+    isLoading(loading = true){
+        if(loading === true){
+            let load = document.createElement("h1");
+            load.setAttribute("id", "loading");
+            load.textContent = "...carregando...";
+            this.list.appendChild(load);
+        }
+        else{
+            let load = document.getElementById("loading").remove;
+        };
+    };
     registerHandler(){
         // ao submeter o formulario instaciara o metodo
         this.form.onsubmit = event => this.addRepository(event);
     };
-    addRepository(event){
+    async addRepository(event){
         // ao ser submetido o formulario nao recarregara a pagina
         event.preventDefault();
 
-
-        // this.repositorio recebera o seguinte objeto
-        this.repositorio.push({
-            nome: "P-L-T-S",
-            description: "Alguma coisa que eu não sei oq é pq to com preguiça",
-            avatar_url: "https://avatars0.githubusercontent.com/u/57147101?v=4",
-            html_url: "https://api.github.com/users/P-L-T-S"
-        });
-        // exibe o repositorio no console
-        console.log(this.repositorio);
-        this.render();
+        const intValue = this.repoUserInput.value;
+        console.log(intValue)
+        const repository = this.repository.value;
+        console.log(repository)
+        if(intValue.length === 0)
+            return;
+        this.isLoading();
+            try{
+            const response = await api.get(`/repos/${intValue}/${repository}`);
+            let {name, description, html_url, owner:{avatar_url}} = response.data;
+            // this.repositorio recebera o seguinte objeto
+            this.repositorio.push({
+                name,
+                description,
+                avatar_url,
+                html_url,
+            });
+            this.isLoading(false)
+            this.render();
+        }catch(error){
+            console.warn(error);
+            alert(`Erro, favor verificar o usuario "${intValue}"`);
+            this.isLoading(false);
+        }
     };
+    async addUser(){
+         // ao ser submetido o formulario nao recarregara a pagina
+         event.preventDefault();
+
+         const intValue = this.repoUserInput.value;
+         if(intValue.length === 0)
+             return;
+         this.isLoading();
+             try{
+             const response = await api.get(`/user/${intValue}`);
+             let {name, description, html_url, owner:{avatar_url}} = response.data;
+             // this.repositorio recebera o seguinte objeto
+             this.repositorio.push({
+                 name,
+                 description,
+                 avatar_url,
+                 html_url,
+             });
+             this.isLoading(false)
+             this.render();
+         }catch(error){
+             console.warn(error);
+             alert(`Erro, favor verificar o usuario "${intValue}"`);
+             this.isLoading(false);
+         }
+    }
     render(){
         this.list.innerHTML = "";
         
         this.repositorio.forEach(item => {
-            let {nome, description, avatar_url, html_url} = item;
+            let {name, description, avatar_url, html_url} = item;
 
             // cria um item para cada item no repositorio
             this.createItem = document.createElement("li");
@@ -50,7 +105,7 @@ class App{
             // cria o titulo do item
             let title = document.createElement("strong");
             // insere um valor dentro da titulo
-            title.textContent = nome;
+            title.textContent = name;
 
             // cria a tag p
             let text = document.createElement("p");
@@ -75,7 +130,7 @@ class App{
             this.createItem.appendChild(link);
         })
         
-
+    this.repoUserInput.value = "";
     }
 };
 // instancia a classe
